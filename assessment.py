2f12c8c1b27912d4f8139f4fdf2736c94afa02ed
Talal <hijje.sw@gmail.com>
2025-12-01 16:38:00 +0000

    Add more shit

---
+++ assessment.py
@@ -0,0 +1,13 @@
+import string
+import secrets
+import hashlib
+def generate_and_hash_keys():
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(256))
+    hash1 = hashlib.sha256(password.encode(), usedforsecurity=True)
+    return password, hash1.hexdigest()
+    
+
+password, hashed_password = generate_and_hash_keys()
+print("Generated Password:", password)
+print("Hashed Password:", hashed_password)
---
+++ encrypt_and_decrypt.py
@@ -0,0 +1,29 @@
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+def encrypt(plaintext, key):
+    iv = os.urandom(32)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend = default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(plaintext)+padder.finalize()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+    return iv, ciphertext
+
+def decrypt(ciphertext, key):
+
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded = decryptor.update (ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(decrypted_padded) + unpadder.finalize() 
+    
+    return plaintext
+key = os.urandom(32)
+plaintext = (b"Hello Everyone! This is a secret message.")
+iv, ciphertext = encrypt(plaintext, key)
+print("Ciphertext:", ciphertext)
+decrypted_plaintext = decrypt(ciphertext, key)
+print("Decrypted Plaintext:", decrypted_plaintext)
