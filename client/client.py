66d22dc9eda49c18c28c1012be10c7e571ed67de
KiwiDux <cianbyron@gmail.com>
2025-12-03 21:58:43 +0000

    Rewriting certain stuff for linux

--- client/client.py
+++ client/client.py
@@ -1,60 +1,23 @@
-import socket
-import os
-import logging
-import time
+import socket, os, logging, time
 from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Hash import SHA512
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_v1_5
 from Crypto.Random import get_random_bytes
-from logging.handlers import RotatingFileHandler
-from sys import stdout
 from datetime import datetime
 
-#---------------------------------------------------------
-# Check List
-# Double check all these are correct
-#---------------------------------------------------------
-# AES-256 (32-byte key)
-# RSA-2048 for key exchange
-# SHA-512 for digital signatures
-# Integrity verification (SHA-512)
-# Digital signature on each log file
-# Hybrid encryption (AES for data, RSA for key)
-# AES-GCM for authenticated encryption
-# Manual send option
 
 
-#---------------------------------------------------------
-# Partially done
-#---------------------------------------------------------
-# Secure storage of private key
-# Secure transport channel	End-to-End but there is no TLS for socket protection
-# Automated daily scheduled logging
-# Automatic log file scanning
-
-
-#---------------------------------------------------------
-# Not Done
-#---------------------------------------------------------
-# PKI, certificate-based public key management
-# Periodic key changes (rotation)
-# Automated daily scheduled sending
-# Structured communication protocol
-# Log report metadata (timestamp, hostname, file name)
-# Secure server IP discovery
-
-
-#---------------------------------------------------------
-# Clear Screen
-#---------------------------------------------------------
 def clear_screen():
     os.system('cls')
 
 
-#---------------------------------------------------------
+def read_logs(self):
+        with open("/var/log/syslog", "rb") as f:
+            return f.read()
+
+
 # Digital Signature (SHA-512, RSA)
-#---------------------------------------------------------
 def file_signature(private_key, file_data):
 	hashed_object = SHA512.new(file_data)
 	signpkcs = PKCS1_v1_5.new(private_key)
@@ -62,27 +25,21 @@ def file_signature(private_key, file_data):
 	return sig
 
 
-#---------------------------------------------------------
 # Encrypt the AES key with RSA (SHA-512)
-#---------------------------------------------------------
 def aes_key_encryption(public_key, aes_key):
 	rsa_cipher = PKCS1_OAEP.new(public_key, hashAlgo=SHA512)
 	key_encrypted = rsa_cipher.encrypt(aes_key)
 	return key_encrypted
 
 
-#---------------------------------------------------------
 # Encrypt the file with AES-256-GCM
-#---------------------------------------------------------
 def aes_file_encryption(aes_key, file_data):
 	aes_cipher = AES.new(aes_key, AES.MODE_GCM)  # GCM uses a nonce
 	ciphertext, tag = aes_cipher.encrypt_and_digest(file_data)
 	return ciphertext, tag, aes_cipher.nonce
 
 
-#---------------------------------------------------------
 # Logging schedule
-#---------------------------------------------------------
 def generate_logs():
 	log_dir = 'LOGS'
 	# create directory if needed
@@ -118,9 +75,7 @@ def generate_logs():
 	return filepath
 
 
-#---------------------------------------------------------
 # Log Directory scan and collection
-#---------------------------------------------------------
 def log_gather(filepath):
 	log_data_list = []
 	old_log_list = []
@@ -128,10 +83,8 @@ def log_gather(filepath):
 	
 	tracking_file = 'processed_logs.txt'
 
-	# Ensure LOGS directory exists
-	if not os.path.exists('LOGS'):
-		print('Directory ', 'LOGS', ' not found.')
-		return log_data_list
+	# directory call back
+	current_logs = read_logs()
 
 	# Get current list of files in LOGS
 	current_logs = [file for file in os.listdir('LOGS') if os.path.isfile(os.path.join('LOGS', file))]
@@ -169,9 +122,7 @@ def log_gather(filepath):
 	return log_data_list
 
 
-#---------------------------------------------------------
 # Encrypt, sign logs
-#---------------------------------------------------------
 def encrypt_logs(log_data_list):
 	if not log_data_list:
 		return None
@@ -213,9 +164,7 @@ def encrypt_logs(log_data_list):
 	return encrypted_aes_key, encrypted_file_data, tag, nonce, sig
 
 
-#---------------------------------------------------------
 # Start the client connection
-#---------------------------------------------------------
 def open_socket(encrypted_aes_key, encrypted_file_data, tag, nonce, sig):
 	#input_serverip = input('Enter the server IP address: ')
 	#input_serverport = input('Enter the server port: ')
@@ -288,6 +237,16 @@ def process_log_cycle():
 		print('\nEncryption failed.')
 
 
+def auto_send(self):
+        print(" Auto_send started. Logs will be sent at 17:00 every day.")
+        while True:
+            now = datetime.now()
+            if now.hour == 17 and now.minute == 0:
+                print('\n', now.strftime('%H:%M:%S'), ' Scheduled send triggered!')
+                self.send_logs()
+                time.sleep(60)
+
+
 def main():
 	
 	print('\n## Program started at', datetime.now(), ' ##')
@@ -299,16 +258,10 @@ def main():
 		process_log_cycle()
 
 	elif menu_select == '2':
-		print('Auto Log Send started. Press Any Key to stop.')
-		try:
-			while True:
-				process_log_cycle()
-				print('\nWaiting 2 seconds...')
-				time.sleep(2)
-		except KeyboardInterrupt:
-			print('\nAuto send stopped by user.')
-		except Exception as error:
-			print('An error occurred: ', error)
+		print('Auto Send started. Press Any Key to stop.')
+		auto_send()
+	elif menu_select == '3':
+		print('Exiting program.')
 
 	else:
 		print('Invalid selection!')
