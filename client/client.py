c85b567b21e82aad170e3ed01fc86baabaee1b38
KiwiDux <cianbyron@gmail.com>
2025-12-04 10:34:16 +0000

    test

--- client/client.py
+++ client/client.py
@@ -217,7 +217,7 @@ def main():
 	menu_select = input(string_menu)
 
 	if menu_select == '1':
-		client.process_log_cycle()
+		client.process_log_cycle(client.log_dir)
 
 	elif menu_select == '2':
 		print('Automatic log sending (17:00 daily). Press Any Key to stop.')
