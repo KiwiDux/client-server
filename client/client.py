51ab3a98350654de3f368c0835575f0355e5d6c4
1chthyosaur <grace@clowrey.co.uk>
2025-12-11 12:49:53 +0000

    comments and var names

--- client/client.py
+++ client/client.py
@@ -11,53 +11,53 @@ from datetime import datetime
 
 class Client:
 	def __init__(self):
-		#self.server_ip = input('Enter the server IP address: ')
-		#self.server_port = int(input('Enter the server port: '))
+		# self.server_ip = input('Enter the server IP address: ')
+		# self.server_port = int(input('Enter the server port: '))
 		self.server_ip = '192.168.200.4'
 		self.server_port = 1234
 
+	# Generate AES keys
 	def key_generation(self):
-		if not os.path.exists('client_private_key.pem'):
-			print('Generating Keys...')
-			key = RSA.generate(2048)
+		if not os.path.exists('client_private_key.pem'): # path to the client's private key
+			print('Generating Keys...') 
+			key = RSA.generate(2048) # generate key with AES
 			open('client_private_key.pem', 'wb').write(key.export_key())
 			open('client_public_key.pem', 'wb').write(key.publickey().export_key())
-		
 		print('Keys generated.')
 	
-	def receive_exact(self, sock, length):
-		data = b''
-		while len(data) < length:
-			chunk = sock.recv(length - len(data))
-			if not chunk:
+	# Receive and return the data received over the socket connection
+	def socket_receive(self, sock, length):
+		binary_data = b''
+		while len(binary_data) < length:
+			socket_chunk = sock.recv(length - len(binary_data))
+			if not socket_chunk:
 				raise ConnectionError('Connection closed')
-			data += chunk
-		return data
+			binary_data += socket_chunk
+		return binary_data 
 
-	def	load_keys(self):
-		self.key_generation()
+	def	loading_keys(self):
+		self.key_generation() 
 		self.client_private_key = RSA.import_key(open('client_private_key.pem', 'rb').read())
 		print('Loaded client private key.')
 	
-	def read_logs(self): # Read Logs
-		with open('/var/log/syslog', 'rb') as f:
-			return f.read()
+	# Reading Ubuntu syslog files (as binary)
+	def reading_logs(self): 
+		with open('/var/log/syslog', 'rb') as f: 
+			return f.read() 
     	
 	# Digital Signature (SHA-512, RSA)
-	def sign_logfile(self, file_data):
+	def signing_log_files(self, file_data):
 		hashed_object = SHA512.new(file_data)
-		signpkcs = PKCS1_v1_5.new(self.client_private_key)
-		sig = signpkcs.sign(hashed_object)
+		sign_pkcs = PKCS1_v1_5.new(self.client_private_key)
+		sig = sign_pkcs.sign(hashed_object)
 		return sig
 
 	# Encrypt, sign logs
-	def encrypt_logs(self, log_file):
+	def encrypting_logs(self, log_file):
 		# Generate a random AES key for symmetric encryption
-		aes_key = get_random_bytes(32)  # AES-256
+		aes_key = get_random_bytes(32)  # AES-256 key generation
 		print('AES key generated.')
-
 		aes_cipher = AES.new(aes_key, AES.MODE_GCM)  # GCM uses a nonce
-
 		ciphertext, tag = aes_cipher.encrypt_and_digest(log_file)
 
 		return ciphertext, tag, aes_cipher.nonce, aes_key
@@ -68,14 +68,8 @@ class Client:
 		key_encrypted = rsa_cipher.encrypt(aes_key)
 		return key_encrypted
 
-	def send_data(self, encrypted_data):
-		aes_key, encrypted_file_data, tag, nonce, sig = encrypted_data
-		
-		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		client_socket.connect((self.server_ip, self.server_port))
-		
-
-	def send_data(self, encrypted_data):
+	# Sending encrypted data over the socket
+	def sending_data(self, encrypted_data):
 		aes_key, encrypted_file_data, tag, nonce, sig = encrypted_data
 
 		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -84,7 +78,7 @@ class Client:
 		try:
 			# Receive server public key
 			server_key_length = struct.unpack('>I', client_socket.recv(4))[0]
-			server_key_bytes = self.receive_exact(client_socket, server_key_length)
+			server_key_bytes = self.socket_receive(client_socket, server_key_length)
 			self.server_public_key = RSA.import_key(server_key_bytes)
 			print('Received server public key')
 			
@@ -124,38 +118,42 @@ class Client:
 			print('\nAuto send stopped by user.')
 			self.client_socket.close()
 
-
-	def send_logs(self):
+	# Connect to client and send encypted logs
+	def sending_logs(self):
 		'''Start the client connection'''
 		print('Connecting to ', self.server_ip, ':', self.server_port, '...')
+		
 		try:
-			self.load_keys()
-			logs = self.read_logs()
+			self.loading_keys()
+			logs = self.reading_logs()
 			print(f'Read {len(logs)} bytes from log file.')
 
-			signature = self.sign_logfile(logs)
+			signature = self.signing_log_files(logs)
 			print('Log file signed.')
 
-			encrypted_file_data, tag, nonce, aes_key = self.encrypt_logs(logs)
+			encrypted_file_data, tag, nonce, aes_key = self.encrypting_logs(logs)
 			print('Log file encrypted.')
 
 			encrypted_data = (aes_key, encrypted_file_data, tag, nonce, signature)
 			print('AES key encrypted with RSA')
-			self.send_data(encrypted_data)
+			self.sending_data(encrypted_data)
 		
 		except Exception as e:
-			print('Error during sending:', e)
+			print('Error during sending: ', e)
 
-	def send_manually(self):
-		print(' Manual log send started.')
-		self.send_logs()
+	# Manual log sending
+	def sending_manually(self):
+		print('Manual log sending started.')
+		self.sending_logs()
 
-	def auto_send(self):
-		print('Auto_send started. Logs will be sent at 17:00 every day.')
+	# Automatic log sending
+	def auto_sending(self):
+		print('Auto_sending started. Logs will be sent at 17:00 every day.')
+		
 		while True:
-			now = datetime.now()
-			if now.hour == 17 and now.minute == 0:
-				print('\n', now.strftime('%H:%M:%S'), ' Scheduled send triggered!')
+			time_now = datetime.now()
+			if time_now.hour == 17 and time_now.minute == 0:
+				print('\n', time_now.strftime('%H:%M:%S'), ' Scheduled send triggered!')
 				self.send_logs()
 				time.sleep(60)
 			else:
@@ -165,20 +163,19 @@ def main():
 	client = Client()
 	print('\n## Program started at', datetime.now(), ' ##')
 	
-	string_menu = '\n1.\tManual Log Send.\n2.\tAuto Log Send at 17:00.\n\nSelect an option from above: '
-	
+	menu_string = '\n1.\tManual Log Send.\n2.\tAuto Log Send at 17:00.\n\nSelect an option from above: '
 	while True:
-		menu_select = input(string_menu)
+		
+		menu_selection = input(menu_string)
+		if menu_selection == '1':
+			client.sending_manually()
 
-		if menu_select == '1':
-			client.send_manually()
-
-		elif menu_select == '2':
-			auto_thread = threading.Thread(target=client.auto_send, daemon=True)
+		elif menu_selection == '2':
+			auto_thread = threading.Thread(target=client.auto_sending, daemon=True)
 			auto_thread.start()
 			print('Automatically sending logs at 17:00 daily.')
 
-		elif menu_select == '3':
+		elif menu_selection == '3':
 			print('Exiting program.')
 			break
 
